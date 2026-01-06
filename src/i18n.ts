932b30af895069ef1061569650e7d86922f614d3
yohannis <Abayyohannis2014@gmail.com>
2026-01-06 13:25:21 +0300

    update

--- src/i18n.ts
+++ src/i18n.ts
@@ -74,7 +74,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: 'en', // language to use, more info here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: 'am', // language to use, more info here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
